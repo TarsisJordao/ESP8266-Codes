7a338f761ec81e3fa34b16ebba6ca5b92c611e53
TarsisJordao <38726644+TarsisJordao@users.noreply.github.com>
2018-06-20 10:15:47 -0300

    Add files via upload

---
+++ Calibragem_EntradadaADCv1.5.ino
@@ -0,0 +1,154 @@
+#define portaADC 0
+#define tempo 1000
+
+double leituraAnalogica[6],matValADC[100];
+
+const int matrizAuxiliar[6]={0,2,4,6,8,10};
+
+volatile int i,j,somaAux;
+
+double tensao,coefAngular,coefLinear,valorADC_medio;
+
+char aux;
+
+double somatorioAD,somatorioV,mediaSomatorioAD,mediaSomatorioV,somatorioProdutoADV,somatorioAD_Quadrado,Sxy,Sxx;
+
+void setup()
+{
+  Serial.begin(115200);
+}
+
+void loop()
+{
+//Coleta de valores Analogicos 
+//=============================================================================================================================================================================================
+  j=0;
+  
+  while(j<6)
+  {   
+    if(Serial.available() >= 0)
+    {
+      aux=Serial.read();
+      
+      if(aux=='0')
+      {
+             
+        Serial.println();
+        Serial.print("Ajuste o valor de tensao de ");
+        Serial.print(matrizAuxiliar[j]);
+        Serial.print("V na entrada analogica");
+        Serial.println();
+
+        somaAux=0;
+        
+        for(i=0; i<100; i++)
+        {
+          matValADC[i]= analogRead(portaADC);
+          delay(10);
+        }
+        
+        for(i=0; i<100; i++)
+        {
+          somaAux+= matValADC[i];
+        }
+        
+        valorADC_medio=somaAux/(i*1.0);
+      
+        leituraAnalogica[j]=valorADC_medio;
+        
+        Serial.print("O valor na entradada analogica correspondente a ");
+        Serial.print(matrizAuxiliar[j]);
+        Serial.print("V eh de:");
+        Serial.println(leituraAnalogica[j]);
+      
+        delay(tempo);
+        j++;
+      }
+    }
+  }
+//=============================================================================================================================================================================================
+//=============================================================================================================================================================================================
+
+//CALCULO DE REGRESSÃO LINEAR
+
+  somatorioAD=0;
+  somatorioV=0;
+  somatorioProdutoADV=0;
+  somatorioAD_Quadrado=0;
+
+  for(j=0;j<6;j++)
+  {
+    somatorioAD+=leituraAnalogica[j];
+    somatorioV+=matrizAuxiliar[j];
+    somatorioProdutoADV+=leituraAnalogica[j]*matrizAuxiliar[j];
+    somatorioAD_Quadrado+=leituraAnalogica[j]*leituraAnalogica[j];
+  }
+
+  mediaSomatorioAD=somatorioAD/(j*1.0);
+  mediaSomatorioV=somatorioV/(j*1.0);
+
+  Sxy=somatorioProdutoADV-(j*mediaSomatorioAD*mediaSomatorioV);
+  Sxx=somatorioAD_Quadrado-(j*mediaSomatorioAD*mediaSomatorioAD);
+
+  coefAngular=Sxy/Sxx;
+  coefLinear=mediaSomatorioV-(coefAngular*mediaSomatorioAD);
+
+//ESCREVE INFOS NA SERIAL
+//=============================================================================================================================================================================================  
+      for(j=0;j<6;j++)
+      {
+        Serial.print("O valor analogico correspondente a tensao de ");
+        Serial.print(matrizAuxiliar[j]);
+        Serial.print("V eh de:");
+        Serial.println(leituraAnalogica[j]);
+      }
+      
+      Serial.print("Somatorio dos valores Analogicos eh:");
+      Serial.println(somatorioAD);
+      Serial.print("Somatorio das Tensoes:");
+      Serial.println(somatorioV);
+      Serial.print("Media do somatorio dos valores Analogicos:");
+      Serial.println(mediaSomatorioAD);
+      Serial.print("Media do somatorio dos valores das Tensoes:");
+      Serial.println(mediaSomatorioV);
+      Serial.print("Somatorio do produto do valor Analogico x Tensao:");
+      Serial.println(somatorioProdutoADV);
+      Serial.print("Somatorio do quadrado dos valores Analogicos:");
+      Serial.println(somatorioAD_Quadrado);
+      Serial.print("Fator Sxy:");
+      Serial.println(Sxy);
+      Serial.print("Fator Sxx:");
+      Serial.println(Sxx);
+      Serial.println();
+      
+  while(1)
+  {
+        somaAux=0;
+        
+        for(i=0; i<100; i++)
+        {
+          matValADC[i]= analogRead(portaADC);
+          delay(10);
+        }
+        
+        for(i=0; i<100; i++)
+        {
+          somaAux+= matValADC[i];
+        }
+        
+        valorADC_medio=somaAux/100;
+            
+      tensao=coefAngular*valorADC_medio + coefLinear;
+
+          
+      Serial.print("O coeficiente Angular eh:");
+      Serial.println(coefAngular);
+      Serial.print("O coeficiente Linear eh:");
+      Serial.println(coefLinear);
+      Serial.print("O valor de tensao em Volts eh:");
+      Serial.println(tensao);
+      Serial.println();
+      delay(5000);
+    }
+//=============================================================================================================================================================================================
+}
---
+++ Teste_GPIO_ESP8266.ino
@@ -0,0 +1,178 @@
+//Sketch para teste de entradas digitais do ESP8266-07
+
+#define GPIO4   4
+#define GPIO5   5
+#define GPIO12 12
+#define GPIO13 13
+
+const int GPIO[4]={4,5,12,13};
+int i;
+
+char charRead;
+
+void setup() 
+{
+  Serial.begin(4800);
+
+  for(i=0;i<=3;i++)
+    pinMode(GPIO[i],INPUT);
+  
+  inicio();
+}
+
+void inicio()
+{
+  Serial.println("TESTE DE ENTRADAS DIGITAIS DO ESP8266-07 COM RETORNO NA SERIAL");
+  Serial.println();
+  Serial.println("Selecione qual GPIO sera testado:");
+  Serial.println();
+  Serial.println("1 - GPIO4");
+  Serial.println("2 - GPIO5");
+  Serial.println("3 - GPIO12");
+  Serial.println("4 - GPIO13");
+  Serial.println("5 - Para identificar a GPIO acionada automaticamente");
+  Serial.println();
+}
+
+void loop() 
+{
+  volatile int flag=1;
+  
+  while(Serial.available() > 0)
+  {
+    charRead=Serial.read();
+
+    switch(charRead)
+    {
+      case '1':
+      {
+        Serial.println();
+        Serial.println("GPIO4 Selecionado:");
+        Serial.println("Acione a entrada digital correspondente ao GPIO4");
+        delay(4000);
+            
+           if(digitalRead(GPIO4)==0)
+           {
+             Serial.println();
+             Serial.println("A entrada digital esta funcionando");
+             Serial.println();
+           }
+          
+            else
+            {
+              Serial.println();
+              Serial.println("A entrada digital nao foi acionada");
+              Serial.println();
+            }
+        
+      break;
+      }
+      
+      case '2':
+      {
+        Serial.println();
+        Serial.println("GPIO5 Selecionado:");
+        Serial.println("Acione a entrada digital correspondente ao GPIO5");
+        delay(4000);
+            
+           if(digitalRead(GPIO5)==0)
+           {
+             Serial.println();
+             Serial.println("A entrada digital esta funcionando");
+             Serial.println();
+           }
+          
+            else
+            {
+              Serial.println();
+              Serial.println("A entrada digital nao foi acionada");
+              Serial.println();
+            }
+      break;
+      }
+      
+      case '3':
+      {
+        Serial.println();
+        Serial.println("GPIO12 Selecionado:");
+        Serial.println("Acione a entrada digital correspondente ao GPIO12");
+        delay(4000);
+            
+           if(digitalRead(GPIO12)==0)
+           {
+             Serial.println();
+             Serial.println("A entrada digital esta funcionando");
+             Serial.println();
+           }
+          
+            else
+            {
+              Serial.println();
+              Serial.println("A entrada digital nao foi acionada");
+              Serial.println();
+            }
+      
+      break;
+      }
+      
+      case '4':
+      {
+        Serial.println();
+        Serial.println("GPIO13 Selecionado");
+        Serial.println("Acione a entrada digital correspondente ao GPIO13");
+        delay(4000);
+            
+           if(digitalRead(GPIO13)==0)
+           {
+             Serial.println();
+             Serial.println("A entrada digital esta funcionando");
+             Serial.println();
+           }
+          
+            else
+            {
+              Serial.println();
+              Serial.println("A entrada digital nao foi acionada");
+              Serial.println();
+            }
+        
+      break;
+      }
+
+      case '5':
+      {
+        Serial.println();
+        Serial.println("Acione a GPIO desejada:");
+        Serial.println();
+        delay(4000);
+        
+        for(i=0;i<=3;i++)
+        {
+          if(digitalRead(GPIO[i])==0)
+          {
+            Serial.print("A entrada GPIO");
+            Serial.print(GPIO[i]);
+            Serial.println(" foi acionada");
+            Serial.println();
+
+            flag=0;
+          }
+        }
+        if(flag==1)
+        {
+          Serial.println("Nenhuma entrada GPIO foi acionada");
+          Serial.println();
+        }
+      
+      break;
+      }
+
+      default:
+      {
+        Serial.println("Opcao invalida");
+        Serial.println();
+      }
+      
+    }
+  }
+}
